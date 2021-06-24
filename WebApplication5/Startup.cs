882d96d19961f7d43783fd4313eda30fd043f6a8
Renish Vekariya <PCA112@internal.mail>
2021-06-24 17:45:59 +0530

    update

--- WebApplication5/Startup.cs
+++ WebApplication5/Startup.cs
@@ -32,7 +32,7 @@ namespace WebApplication5
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World     ,,,,,!");
                 });
             });
         }
